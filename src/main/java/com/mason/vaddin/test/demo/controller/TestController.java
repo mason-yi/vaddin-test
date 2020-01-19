c2e06ee0316242ef71c4e95a348bfda920f00a9a
Mason <mason.yi@kakaocorp.com>
2020-01-19 21:55:03 +0900

    second commit

---
+++ src/main/java/com/mason/vaddin/test/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.mason.vaddin.test.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/{number}")
+    public Long get(@PathVariable Long number) {
+        return number;
+    }
+}
---
+++ src/main/java/com/mason/vaddin/test/demo/view/MainView.java
@@ -0,0 +1,21 @@
+//package com.mason.vaddin.test.demo.view;
+//
+//import com.vaadin.flow.component.button.Button;
+//import com.vaadin.flow.component.html.Paragraph;
+//import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+//import com.vaadin.flow.component.textfield.TextField;
+//
+//public class MainView extends VerticalLayout {
+//
+//    public MainView() {
+//        TextField name = new TextField("Name");
+//        Paragraph greeting = new Paragraph("");
+//
+//        Button button = new Button("Greet", event -> {
+//            greeting.setText("Hello " + name.getValue());
+//        });
+//
+//        add(name, button, greeting);
+//    }
+//
+//}
